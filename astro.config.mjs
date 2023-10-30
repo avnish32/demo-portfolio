d76cfd84df358d41d8ed0e3db261984f51deca97
avnish32 <avnishbansal9@gmail.com>
2023-10-30 15:20:01 +0000

    Changes to github workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,7 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap(), mdx()]
+  integrations: [tailwind(), sitemap(), mdx()],
+  site: 'https://avnish32.github.io',
+  base: '/demo-portfolio',
 });
